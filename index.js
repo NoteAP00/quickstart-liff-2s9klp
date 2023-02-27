7a90fd89ac546f09d0a81b85578458547dbc22f3
NoteAP00 <110073920+NoteAP00@users.noreply.github.com>
2023-02-27 14:52:47 +0700

    2 files modified

--- index.js
+++ index.js
@@ -36,17 +36,27 @@ switch (liff.getOS()) {
 
 if (!liff.isInClient()) {
   if (liff.isLoggedIn()) {
+    btnShare.style.display = "block"
     btnLogIn.style.display = "none"
     btnLogOut.style.display = "block"
     getUserProfile()
+    getFriendship()
   } else {
     btnLogIn.style.display = "block"
     btnLogOut.style.display = "none"
   }
 }else{
+  btnShare.style.display = "block"
+  btnSend.style.display = "block"
   getUserProfile()
+  getFriendship()
 }
-
+btnOpenWindow.style.display = "block"
+if (liff.isInClient() && liff.getOS() === "android") {
+  btnScanCode.style.display = "block"
+}else{
+  btnScanCode.style.display = "none"
+}
 }
 async function getUserProfile() {
   const profile = await liff.getProfile()
@@ -56,7 +66,45 @@ async function getUserProfile() {
   displayName.innerHTML = "<b>displayName:</b> " + profile.displayName
   email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email
 }
-
+async function sendMsg() {
+  if (liff.getContext().type !== "none" && liff.getContext().type !== "external") {
+    await liff.sendMessages([
+      {
+        "type": "text",
+        "text": "This message was sent by sendMessages()"
+      }
+    ])
+    //alert("Message sent")
+    liff.closeWindow()
+  }
+}
+async function shareMsg() {
+  await liff.shareTargetPicker([
+    {
+      type: "image",
+      originalContentUrl: "https://d.line-scdn.net/stf/line-lp/2016_en_02.jpg",
+      previewImageUrl: "https://d.line-scdn.net/stf/line-lp/2016_en_02.jpg"
+    }
+  ])
+}
+async function scanCode() {
+  const result = await liff.scanCode()
+  code.innerHTML = "<b>Code: </b>" + result.value
+}
+async function getFriendship() {
+  let msg = "Hooray! You and our chatbot are friend."
+  const friend = await liff.getFriendship()
+  if (!friend.friendFlag) {
+     msg = "<a href=\"https://line.me/R/ti/p/@184jxxpa\">Follow our chatbot here!</a>"
+  }
+  friendShip.innerHTML = msg;
+}
+btnOpenWindow.onclick = () => {
+  liff.openWindow({
+    url: window.location.href,
+    external: true
+  })
+}
 
 main()
 btnLogIn.onclick = () => {
@@ -67,6 +115,12 @@ btnLogOut.onclick = () => {
   liff.logout()
   window.location.reload()
 }
-
-
-
+btnSend.onclick = () => {
+  sendMsg()
+}
+btnShare.onclick = () => {
+  shareMsg()
+}
+btnScanCode.onclick = () => {
+  scanCode()
+}
